8a1eb21fd6e20051e1d04379e170c7dd1db43fab
Hammurabi <38507441+Hammurabi@users.noreply.github.com>
2022-01-02 19:18:49 +0100

    Create WolkenException.java

---
+++ src/main/java/io/adenium/exceptions/WolkenException.java
@@ -0,0 +1,14 @@
+package io.adenium.exceptions;
+
+public class WolkenException extends Exception {
+    private static final long serialVersionUID = 5112431926980310096L;
+
+    public WolkenException(String msg)
+    {
+        super(msg);
+    }
+    public WolkenException(Throwable cause)
+    {
+        super(cause);
+    }
+}
