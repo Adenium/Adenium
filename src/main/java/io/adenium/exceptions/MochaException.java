57cc82bcb931cc66e5c98699d07f17fe20d08683
Hammurabi <38507441+Hammurabi@users.noreply.github.com>
2022-01-02 19:18:38 +0100

    Create MochaException.java

---
+++ src/main/java/io/adenium/exceptions/MochaException.java
@@ -0,0 +1,14 @@
+package io.adenium.exceptions;
+
+public class MochaException extends Exception {
+    private static final long serialVersionUID = 5112431926980310096L;
+
+    public MochaException(String msg)
+    {
+        super(msg);
+    }
+    public MochaException(Throwable cause)
+    {
+        super(cause);
+    }
+}
