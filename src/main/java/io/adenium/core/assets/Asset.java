b4bc374f1a7e8bc3d5fad5ab25812f302e4e2709
Hammurabi <38507441+Hammurabi@users.noreply.github.com>
2023-01-29 19:13:17 +0100

    Update Asset.java

--- src/main/java/io/adenium/core/assets/Asset.java
+++ src/main/java/io/adenium/core/assets/Asset.java
@@ -1,48 +1,104 @@
 package io.adenium.core.assets;
 
+import io.adenium.core.transactions.Transaction;
+import io.adenium.exceptions.InvalidAssetImplementation;
 import io.adenium.serialization.SerializableI;
 import io.adenium.serialization.SerializationFactory;
-import io.adenium.core.Address;
 import io.adenium.exceptions.AdeniumException;
+import io.adenium.utils.HashUtil;
+import io.adenium.utils.VarInt;
 
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.math.BigInteger;
+import java.nio.charset.StandardCharsets;
 
 public abstract class Asset extends SerializableI {
     public final static int     UniqueIdentifierLength = 20;
     public final static byte    DefaultUUID[] = new byte[UniqueIdentifierLength];
-    // 20 byte unique identifier (hash160 of constructor contract/transaction).
-    private final byte          uuid[];
+
+    // the name of the asset.
+    private String        name;
+    // a short description of the asset.
+    private String        desc;
+    // 32 byte transaction id for creating the asset.
+    private byte          txid[];
 
     // default constructor
-    public Asset(byte uuid[]) {
-        this.uuid       = uuid;
+    public Asset(String name, String desc) throws InvalidAssetImplementation {
+        byte shortNameBytes[] = name.getBytes(StandardCharsets.UTF_8);
+        if (shortNameBytes.length > 32) throw new InvalidAssetImplementation("Short name is too long");
+
+        byte descriptionBytes[] = desc.getBytes(StandardCharsets.UTF_8);
+        if (descriptionBytes.length > 1024) throw new InvalidAssetImplementation("Description is too long");
+
+        this.name       = name;
+        this.desc       = desc;
+        this.txid       = new byte[Transaction.UniqueIdentifierLength];
+    }
+
+    public void setTransactionId(byte txid[]) {
+        this.txid = txid;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return desc;
+    }
+
+    public byte[] getTransactionId() {
+        return txid;
     }
 
     public byte[] getUUID() {
-        return uuid;
+        return HashUtil.hash160(asByteArray());
     }
 
     @Override
     public void write(OutputStream stream) throws IOException, AdeniumException {
-        stream.write(uuid);
+        stream.write(name.length());
+        stream.write(name.getBytes(StandardCharsets.UTF_8));
+        VarInt.writeCompactUInt32(desc.length(), false, stream);
+        stream.write(desc.getBytes(StandardCharsets.UTF_8));
+        stream.write(txid);
+        writeContent(stream);
     }
 
     @Override
     public void read(InputStream stream) throws IOException, AdeniumException {
-        checkFullyRead(stream.read(uuid), Address.RawLength);
+        int length = 0;
+        length = stream.read();
+        if (length < 0) {
+            throw new AdeniumException("invalid name length.");
+        }
+        byte array[] = new byte[length];
+        checkFullyRead(stream.read(array), length);
+        name = new String(array);
+        length = VarInt.readCompactUInt32(false, stream);
+        array = new byte[length];
+        checkFullyRead(stream.read(array), length);
+        desc = new String(array);
+        checkFullyRead(stream.read(txid), txid.length);
+        readContent(stream);
     }
 
     public abstract boolean isTransferable();
     public abstract boolean isFungible();
+    public abstract BigInteger getSupply();
     public abstract BigInteger getTotalSupply();
 
     public abstract void writeContent(OutputStream stream) throws IOException, AdeniumException;
     public abstract void readContent(InputStream stream) throws IOException, AdeniumException;
 
-    public static void register(SerializationFactory serializationFactory) {
-        serializationFactory.registerClass(NonFungibleToken.class, new NonFungibleToken());
+    public static void register(SerializationFactory serializationFactory) throws InvalidAssetImplementation {
+        serializationFactory.registerClass(NonFungibleToken.class, new NonFungibleToken("", "", new byte[32]));
+    }
+
+    public static final byte[] emptyUUID() {
+        return new byte[UniqueIdentifierLength];
     }
 }
